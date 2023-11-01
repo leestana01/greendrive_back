4cdd3f3e340f23d14c0149984bc02223d02490d5
leestana01 <leestana01@naver.com>
2023-11-02 00:08:05 +0900

    [INIT] SpringBoot 2,7,17 & JAVA 11 // Security, MySQL, JPA, Spring Web

---
+++ src/main/java/com/luckyseven/greendrive/GreendriveApplication.java
@@ -0,0 +1,13 @@
+package com.luckyseven.greendrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreendriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreendriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luckyseven/greendrive/GreendriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckyseven.greendrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreendriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
