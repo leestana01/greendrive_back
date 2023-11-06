b53fe0f63a0dc8871c567f3199ce4e1d7c3b33e6
leestana01 <leestana01@naver.com>
2023-11-07 08:41:39 +0900

    [ADD] Space 검색 기능 추가

--- src/main/java/com/luckyseven/greendrive/Repository/SpaceRepository.java
+++ src/main/java/com/luckyseven/greendrive/Repository/SpaceRepository.java
@@ -4,10 +4,16 @@ import com.luckyseven.greendrive.Domain.Review;
 import com.luckyseven.greendrive.Domain.Space;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface SpaceRepository extends JpaRepository<Space, String> {
+    @Query("SELECT s FROM Space s WHERE " +
+            "s.parkName LIKE %:keyword% OR s.address LIKE %:keyword% " +
+            "OR (s.type = :type AND :type IS NOT NULL)")
+    List<Space> findByKeywordOrType(@Param("keyword") String keyword, @Param("type") Integer type);
+
 }
--- src/main/java/com/luckyseven/greendrive/dto/SpaceForMarkersDto.java
+++ src/main/java/com/luckyseven/greendrive/dto/SpaceForMarkersDto.java
@@ -7,13 +7,10 @@ import lombok.ToString;
 
 
 @Data
-@ToString // 디버그용
 // 카카오맵 마커 표시를 위한 전체 반환용 DTO
 public class SpaceForMarkersDto {
 
     private String id;
-
-    private String parkName; // 주차장명
     private Integer type; // 주차장구분 (공영 : 0, 민영 : 1, 가짜(파트너): 2)
 
     private double latitude; // 위도
